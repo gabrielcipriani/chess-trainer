2ce20f3992e55ff20e2e03546c73c6193d03ab3b
Gabriel <gabrielhuete@gmail.com>
2026-09-07 00:45:19 +0100

    Update App component to use state for board management

--- chess-frontend/src/App.tsx
+++ chess-frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { Board } from './Board.tsx';
 import { boardState } from './boardState.ts';
 
 export function App() {
+  const [board, setBoard] = useState(boardState);
+
   return (
     <>
       <h1>Chess Trainer</h1>
-      <Board board={boardState} />
+      <Board board={board} />
     </>
   );
 }
