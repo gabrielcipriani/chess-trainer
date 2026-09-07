4e2866b3d65d28de5548052dbb5d5a90457bf4e0
Gabriel <gabrielhuete@gmail.com>
2026-09-07 13:29:16 +0100

    Display selected square coordinates in App component

--- chess-frontend/src/App.tsx
+++ chess-frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import { Board } from './Board.tsx';
 import { boardState } from './boardState.ts';
+import type { Position } from './types.ts';
 
 export function App() {
   const [board, setBoard] = useState(boardState);
+  const [selectedSquare, setSelectedSquare] = useState<Position|null>(null);
 
   function handleSquareClick(row: number, col: number): void {
-    console.log(row, col);
+    setSelectedSquare({ row, col });
   }
 
   return (
     <>
       <h1>Chess Trainer</h1>
+      {selectedSquare && (
+      <p>({selectedSquare.row}, {selectedSquare.col})</p>
+      )}
       <Board board={board} onSquareClick={handleSquareClick}/>
     </>
   );
